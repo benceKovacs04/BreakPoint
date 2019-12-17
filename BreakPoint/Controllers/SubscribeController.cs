21aa54cb68a522e586648e394fdd336c14a8febb
Bence Kovács <kovacs.ben.p@gmail.com>
2019-12-17 14:16:17 +0100

    initial commit

---
+++ BreakPoint/Controllers/SubscribeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BreakPoint.Controllers
+{
+    [ApiController]
+    [Route("/subscribe")]
+    public class SubscribeController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> SubscribeRSS()
+        {
+
+            return Ok();
+        }
+    }
+}
---
+++ BreakPoint/Services/ApiService.cs
@@ -0,0 +1,30 @@
+﻿using BreakPoint.Interfaces;
+using BreakPoint.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BreakPoint.Services
+{
+    public class ApiService : IApiService<RSS>
+    {
+        private readonly string API_PATH = "https://cloud.feedly.com/v3/search/feeds/?query=";
+        public HashSet<RSS> GetRSSBy(string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        /*private async Task<string> GetData(string query)
+        {
+            HttpClient client = new HttpClient();
+            return await client.GetStringAsync(API_PATH + query);
+        }
+
+        private HashSet<RSS> BuildRSSSet(string jsonData)
+        {
+
+        }*/
+    }
+}
