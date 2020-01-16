d41f7d84fdf20de6472dbaa81b25ae84c16877be
daniel-kalocsay <kdani782@gmail.com>
2020-01-16 15:50:35 +0100

    register button works, restyled buttons

--- BreakPoint/ClientApp/src/components/Navigation/NavigationItems/NavigationItems.js
+++ BreakPoint/ClientApp/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import classes from "./NavigationItems.module.css";
 import NavigationItem from "./NavigationItem/NavigationItem";
 import loginContext from "../../Context/LoginContext";
+import { Link } from "react-router-dom";
 
 export default function NavigationItems() {
     let { stateShowModal, changeStateShowModal } = useContext(loginContext);
@@ -10,7 +11,18 @@ export default function NavigationItems() {
         <ul className={classes.NavigationItems}>
             {stateShowModal}
             <NavigationItem click={changeStateShowModal}>Log In</NavigationItem>
-            <NavigationItem>Register</NavigationItem>
+            <Link to={{ pathname: "/register" }} style={kabbe}>Register</Link>
+
         </ul>
     );
+}
+
+const ehh = () => {
+    // return <Link></Link>
+}
+
+const kabbe = {
+    textDecoration: 'none',
+    color: 'white',
+    marginLeft: "30px",
 }
--- BreakPoint/ClientApp/src/components/Navigation/Toolbar/Toolbar.js
+++ BreakPoint/ClientApp/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,10 +1,15 @@
 import React from "react";
 import classes from "./Toolbar.module.css";
 import NavigationItems from "../NavigationItems/NavigationItems";
+import { Link } from "react-router-dom";
 
 const toolbar = props => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
+        <Link to={{
+            pathname: "/"
+        }}>
+            <div>MENU</div>
+        </Link>
         <nav>
             <NavigationItems />
         </nav>
